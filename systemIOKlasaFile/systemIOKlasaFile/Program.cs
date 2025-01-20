d8215473e0c834afa58003f3f6574e201766d5d9
DavidCuturic5 <david.cuturic5@gmail.com>
2025-01-20 13:09:10 +0100

    Add files via upload

---
+++ systemIOKlasaFile/systemIOKlasaFile/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace systemIOKlasaFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upiši svoje ime: ");
+            string ime = Console.ReadLine();
+
+            string datoteka = "ime.txt";
+
+            if (File.Exists(datoteka))
+            {
+                if (!Directory.Exists("backup"))
+                {
+                    Directory.CreateDirectory("backup");
+                }
+
+                File.Copy(datoteka, @"backup\ime_"
+                    + DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss")
+                    + ".txt");
+
+                File.Delete(datoteka);
+            }
+
+            File.WriteAllText(datoteka, ime);
+
+            Console.ReadKey();
+        }
+    }
+}
